14866e252f1122f93b44808b73bc9cf897897781
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-06-07 12:16:38 -0300

    feat: initial next-auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
+import { Provider as NextAuthProvider } from "next-auth/client";
 
 import "../styles/global.css";
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextAuthProvider session={pageProps?.session}>
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  );
 }
 
 export default App;
