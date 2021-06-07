0d1e669f2c2a73313253657a28127dea396982cc
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-06-07 12:16:13 -0300

    feat: create auth page

---
+++ src/pages/auth.tsx
@@ -0,0 +1,25 @@
+import { FaGithub, FaArrowRight } from "react-icons/fa";
+
+import styles from "../styles/pages/Auth.module.css";
+
+export default function Auth() {
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.backgroundLogo} />
+      <div className={styles.informationsContainer}>
+        <img src="logo-white.svg" alt="move.it" />
+        <section>
+          <h2>Bem-vindo</h2>
+          <div>
+            <FaGithub color="#dcdde0" size={40} />
+            <p>Faça login com seu Github para começar</p>
+          </div>
+        </section>
+        <button type="button">
+          Entrar com Github
+          <FaArrowRight color="#4953b8" size={20} />
+        </button>
+      </div>
+    </div>
+  );
+}
