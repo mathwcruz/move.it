ea525a9053f968d88291d117d2e3cfabe4a16bb2
Matheus da Cruz <matheuswachcruz@gmail.com>
2021-06-08 13:23:06 -0300

    feat: create users table

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,10 +1,12 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { getSession } from "next-auth/client";
+import { FaArrowRight } from "react-icons/fa";
 
 import { SideBarNav } from "../components/SideBarNav";
 
 import styles from "../styles/pages/Leaderboard.module.css";
+import Link from "next/link";
 
 export default function Leaderboard() {
   return (
@@ -15,7 +17,49 @@ export default function Leaderboard() {
       <div className={styles.sideBarNavContainer}>
         <SideBarNav />
         <div className={styles.leaderboardContainer}>
-          <h1>teste</h1>
+          <h2>Leaderboard</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Posição</th>
+                <th>Usuário</th>
+                <th>Desafios</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Matheus da Cruz</td>
+                <td>
+                  <span>130</span> completados
+                </td>
+                <td>
+                  <Link href="/">
+                    <a>
+                      Ver perfil
+                      <FaArrowRight size={14} color="#4953b8" />
+                    </a>
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Jonatan Aguiar</td>
+                <td>
+                  <span>114</span> completados
+                </td>
+                <td>
+                  <Link href="/">
+                    <a>
+                      Ver perfil
+                      <FaArrowRight size={14} color="#4953b8" />
+                    </a>
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
